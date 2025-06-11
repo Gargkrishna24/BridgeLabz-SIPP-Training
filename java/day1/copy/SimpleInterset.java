7df46988a392d7952e791c61bee4e16c1ce3483c
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-11 09:36:59 +0530

    intialise first commit

---
+++ java/day1/copy/SimpleInterset.java
@@ -0,0 +1,12 @@
+package day1.copy;
+import java.util.Scanner;
+public class SimpleInterset {
+	public static void Main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int principal = sc.nextInt();
+		int rate = sc.nextInt();
+		int time = sc.nextInt();
+		System.out.println("Simple Interset ="+ (principal * rate * time)/100);
+	}
+
+}
