b270159862a4a655858c39ecc23f8ffa831d697e
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-12 12:21:48 +0530

    some changes

--- java/java_programming_element_level2/BooleanOperation.java
+++ java/java_programming_element_level2/BooleanOperation.java
@@ -1,5 +1,29 @@
 package java_programming_element_level2;
-
+import java.util.Scanner;
 public class BooleanOperation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		// Input three doubles
+		System.out.print("Enter a (double): ");
+		double a = sc.nextDouble();
+
+		System.out.print("Enter b (double): ");
+		double b = sc.nextDouble();
+
+		System.out.print("Enter c (double): ");
+		double c = sc.nextDouble();
+
+		// Perform operations (same precedence rules, but with floating-point division)
+		double result1 = a + b * c; // * before +
+		double result2 = a * b + c; // * before +
+		double result3 = c + a / b; // / before +
+		double result4 = a % b + c; // % before +
+
+		// Print results
+		System.out.println("The results of Double Operations are " + result1 + ", " + result2 + ", " + result3
+				+ ", and " + result4);
+
+		sc.close();
+	}
 }
--- java/java_programming_element_level2/IntOperation.java
+++ java/java_programming_element_level2/IntOperation.java
@@ -1,5 +1,32 @@
 package java_programming_element_level2;
 
-public class IntOperation {
+import java.util.Scanner;
 
+public class IntOperation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Input three integers
+		System.out.print("Enter a (int): ");
+		int a = sc.nextInt();
+
+		System.out.print("Enter b (int): ");
+		int b = sc.nextInt();
+
+		System.out.print("Enter c (int): ");
+		int c = sc.nextInt();
+
+		// Perform operations (considering precedence)
+		int result1 = a + b * c; // Multiplication (*) has higher precedence than addition (+)
+		int result2 = a * b + c; // Same precedence (* before +), left-to-right
+		int result3 = c + a / b; // Division (/) has higher precedence than addition (+)
+		int result4 = a % b + c; // Modulus (%) has same precedence as division (/), then addition (+)
+
+		// Print results
+		System.out.println(
+				"The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+
+		sc.close();
+
+	}
 }
