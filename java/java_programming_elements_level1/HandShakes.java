b0f64c56a832c5442b6ccb6c4dd27a94c7f7f941
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-12 11:23:52 +0530

    HandShakes

---
+++ java/java_programming_elements_level1/HandShakes.java
@@ -0,0 +1,15 @@
+package java_programming_elements_level1;
+
+import java.util.Scanner;
+
+public class Handshakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = input.nextInt();
+        
+        int handshakes = n * (n - 1) / 2;
+        System.out.println("Maximum number of possible handshakes: " + handshakes);
+        input.close();
+    }
+}
