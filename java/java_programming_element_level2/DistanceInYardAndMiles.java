f55535dce5048a821eaa2cb5dfdcd3629ddf6a90
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-12 10:05:31 +0530

    DistanceInYardAndMiles

---
+++ java/java_programming_element_level2/DistanceInYardAndMiles.java
@@ -0,0 +1,9 @@
+package java_programming_element_level2;
+import java.util.Scanner;
+public class DistanceInYardAndMiles {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int distanceInFeet = sc.nextInt();
+		System.out.println("The distance in yards is"+ ((distanceInFeet/3)/1760)+" while the distance in miles is "+distanceInFeet/3);
+	}
+}
