c6a0cc3a1755a04fb0c5d4d1a8c2142893738668
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-12 09:52:49 +0530

    solve perimeter of a square

---
+++ java/java_programming_element_level2/PerimeterOfSquare.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package java_programming_element_level2;
+
+/**
+ * 
+ */
+public class PerimeterOfSquare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
