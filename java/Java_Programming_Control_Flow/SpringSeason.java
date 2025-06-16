e10cf54f9a8880e9bfe890ced7d88cf5bf4e0262
KRISHNA GARG <124602377+Gargkrishna24@users.noreply.github.com>
2025-06-16 08:54:33 +0530

    Add files via upload
    complete control flow assignment

--- java/Java_Programming_Control_Flow/SpringSeason.java
+++ java/Java_Programming_Control_Flow/SpringSeason.java
@@ -1,5 +1,29 @@
 package Java_Programming_Control_Flow;
-
+import java.util.Calendar;
+import java.util.Scanner;
 public class SpringSeason {
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter month (1-12): ");
+    int month = sc.nextInt();
 
+    System.out.print("Enter day (1-31): ");
+    int day = sc.nextInt();
+	boolean isSpring = false;
+	 if ((month == 3 && day >= 20 && day <= 31) ||
+	            (month == 4 && day >= 1 && day <= 30) ||
+	            (month == 5 && day >= 1 && day <= 31) ||
+	            (month == 6 && day >= 1 && day <= 20)) {
+	            isSpring = true;
+	            
+	 }
+	 if (isSpring) {
+         System.out.println("Yes, it is Spring season!");
+     } else {
+         System.out.println("No, it is not Spring season.");
+     }
+
+     sc.close();
+	}
 }
