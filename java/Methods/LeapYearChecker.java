562a28577270695a4f4dc38fb5826a35c7db4427
GargKrishna24 <gargkrishna@888@gmail.com>
2025-06-19 09:29:18 +0530

    {krishna garg] Add . save achanges

--- java/Methods/LeapYearChecker.java
+++ java/Methods/LeapYearChecker.java
@@ -1,5 +1,15 @@
 package Methods;
 
-public class LeapYearChecker {
+import java.util.Scanner;
 
+public class LeapYearChecker {
+    public static boolean isLeapYear(int year) {
+        return year >= 1582 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(isLeapYear(year) ? "Leap Year" : "Not a Leap Year");
+    }
 }
--- java/Methods/SumOfnNumbers.java
+++ java/Methods/SumOfnNumbers.java
@@ -1,5 +1,15 @@
 package Methods;
-
+import java.util.*;
 public class SumOfnNumbers {
-
+	public static int sumofNnumbers(int n) {
+		int sum = n*(n+1)/2;
+		return sum;
+	}
+	 public static void main(String[] args) {
+	   Scanner sc= new Scanner(System.in);
+	   
+	   int n=sc.nextInt();
+	   int sum = sumofNnumbers(n);
+	   System.out.println(sum);
+	 }
 }
